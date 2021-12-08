a91d25afb7d4bb8ab67f6fd3326fbd88367005e2
LongVo248 <vohoanglongfx@gmail.com>
2021-12-08 14:36:10 +0700

    Hashmap

--- src/Mutable_Immutable/Main1.java
+++ src/Mutable_Immutable/Main1.java
@@ -3,7 +3,7 @@ package Mutable_Immutable;
 public class Main1 {
     public static void main(String[] args){
         Immutable im= new Immutable("vo","Long");
-        // im.first_name="vo"; l
+        // im.first_name="vo"; lỗi
 
         Mutable m= new Mutable("vo hoang","long");
         m.first_name= "vo";
